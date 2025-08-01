28e7bb68f2f39f723629dd697f84a80423552ffb
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2025-08-01 09:45:49 -0300

    fix: changing generic test to a real aplication test

--- frontend/src/hooks/useForm.test.js
+++ frontend/src/hooks/useForm.test.js
@@ -3,20 +3,43 @@ import useForm from './useForm';
 
 describe('useForm', () => {
   test('Inicializa com estado inicial correto', () => {
-    const initialState = { name: '', email: '' };
+    const initialState = {
+      selectedPreferences: [],
+      selectedFeatures: [],
+      selectedRecommendationType: '',
+    };
     const { result } = renderHook(() => useForm(initialState));
 
     expect(result.current.formData).toEqual(initialState);
   });
 
-  test('Atualiza campo corretamente', () => {
-    const initialState = { name: '', email: '' };
+  test('Atualiza selectedPreferences corretamente', () => {
+    const initialState = {
+      selectedPreferences: [],
+      selectedFeatures: [],
+      selectedRecommendationType: '',
+    };
     const { result } = renderHook(() => useForm(initialState));
 
     act(() => {
-      result.current.handleChange('name', 'João');
+      result.current.handleChange('selectedPreferences', ['Integração com chatbots']);
     });
 
-    expect(result.current.formData.name).toBe('João');
+    expect(result.current.formData.selectedPreferences).toEqual(['Integração com chatbots']);
+  });
+
+  test('Atualiza selectedRecommendationType corretamente', () => {
+    const initialState = {
+      selectedPreferences: [],
+      selectedFeatures: [],
+      selectedRecommendationType: '',
+    };
+    const { result } = renderHook(() => useForm(initialState));
+
+    act(() => {
+      result.current.handleChange('selectedRecommendationType', 'SingleProduct');
+    });
+
+    expect(result.current.formData.selectedRecommendationType).toBe('SingleProduct');
   });
 });
